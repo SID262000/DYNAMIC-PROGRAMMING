5055198ffe49d902a040704c9865df4066462bcc
Kumari Ritu Sarsahay <102847337+rsarsahay@users.noreply.github.com>
2023-07-17 14:54:47 +0530

    152.Maximum_Product_Subarray.cpp

--- 152.Maximum_Product_Subarray.cpp
+++ 152.Maximum_Product_Subarray.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
 
+    /* Brute Force 
+        1. Run a loop to find the start of the subarrays.
+        2. Run another nested loop
+        3. Multiply each element and store the maximum value of all the subarray.
+
+        T.C =O(N*N)
+        S.C =O(1)
+    */
+
 
     //https://leetcode.com/problems/maximum-product-subarray/description/
-    /* Intuition :
-       For solving this problem  kadane's algorithm will be the best approach to
-       solve the problem in O(N).
+    /* Optimized Solution
+        Intuition :
+        The intuition of the algorithm is not to consider the subarray as a part of the answer
+        if its sum is less than 0. A subarray with a sum less than 0 will always reduce our
+        answer and so this type of subarray cannot be a part of the subarray with maximum sum.
+        For solving this problem  kadane's algorithm will be the best approach.
 
        Algorithm :
         1.Initially store 0th index value in maxp, minp and r.
